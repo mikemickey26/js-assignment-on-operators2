c1df7ea9475a55a030a740fa4e1acd42ed5598e7
mikemickey26 <107206773+mikemickey26@users.noreply.github.com>
2022-07-31 07:02:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let number = -5
-if (number>=0) {
-    console.log("${number} is positive");
-}else{
-    console.log("${number} is negative");
+//let number = -5
+//if (number>=0) {
+//    console.log("${number} is positive");
+//}else{
+ //   console.log("${number} is negative");
+//}
+function checkifpositive(num) {
+    if (num>=0) {
+            console.log("${num} is positive");
+        }else{
+           console.log("${num} is negative");
+        } 
 }
+checkifpositive(-98)
+checkifpositive(90)
+checkifpositive(-87)
+checkifpositive(69)
