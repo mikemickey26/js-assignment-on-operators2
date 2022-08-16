b1b9b70ac7a08dc67f92fb3c9e495105434b6fae
mikemickey26 <107206773+mikemickey26@users.noreply.github.com>
2022-08-16 06:48:08 -0700

    updated

---
+++ fourth.js
@@ -0,0 +1,12 @@
+//A health care worker is asked to infuse 500 millilitres of medication into a patient over a period of 2 hours.
+//the hospital uses 15-drip sets. what is the drip rate? 
+//find the formula for drip rate and use the formula to solve the question
+
+function med() {
+    let volume = 500ml
+    let time = 2h
+
+    driprate = volume / time
+    console.log(driprate);
+}
+med
--- third.js
+++ third.js
@@ -1,6 +1,11 @@
 //A doctor orders 4 grams of medication for a patient. 
 //The medication is available in capsules of 500 milligrams.
  //How many capsules should be given? Write a function to solve this.
- function checknumofcapsules(params) {
-    let 
+ function med(cap) {
+    let onegram = new Number(1000)
+    let fourgram = onegram * 4
+    let totalnumberofcapsules = fourgram / cap
+    console.log(totalnumberofcapsules);
  }
+ med(500)
+ //the number of capsules the patient should be given is 8
